740242d3c928eb946d3692a40addfa160bd5a525
lucas-pmelo <lucas.pinheiro.melo@gmail.com>
2023-02-08 14:05:06 -0300

    Preparation for project done

---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <img></img>
+      <ul>
+        <li></li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+      <ul>
+        <li></li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+      <ul>
+        <li></li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <img></img>
+      <ul>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+        <li>
+          <a href=""></a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Nav;
